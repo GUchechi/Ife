6fc92c77051e5427517cbae843fb8dbfe4ef5100
GUchechi <godswilluchendu7@gmail.com>
2023-01-27 14:30:22 +0100

    first commit

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -7,23 +7,25 @@ import {
   FaTelegramPlane,
   FaTwitter,
 } from "react-icons/fa";
- 
+
 const Footer = () => {
   return (
-    <div className="footer_section">
-      <div className="logo">
-        <img src={logo2} width="55px" height="55px" alt="logo" />
+    <footer>
+      <div className="footer_section">
+        <div className="logo">
+          <img src={logo2} width="55px" height="55px" alt="logo" />
+        </div>
+        <div className="socialIcons">
+          <FaTwitter size={22} />
+          <FaInstagram size={22} />
+          <FaTelegramPlane size={22} />
+          <FaDiscord size={22} />
+        </div>
+        <div className="copyRight">
+          <p>&copy; ifẹ, 2022</p>
+        </div>
       </div>
-      <div className="socialIcons">
-        <FaTwitter size={22} />
-        <FaInstagram size={22} />
-        <FaTelegramPlane size={22} />
-        <FaDiscord size={22} />
-      </div>
-      <div className="copyRight">
-        <p>&copy; ifẹ, 2022</p>
-      </div>
-    </div>
+    </footer>
   );
 };
 
